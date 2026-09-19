39eec1a753063d002bb6a10aabd98178d2972835
fakeposition <emblemaes@gmail.com>
2026-09-20 00:25:36 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2216,7 +2216,7 @@ do
         SliderInner.InputBegan:Connect(function(Input)
             if Input.UserInputType == Enum.UserInputType.MouseButton1 and not Library:MouseIsOverOpenedFrame() then
                 local mPos = Mouse.X;
-                local gPos = Fill.Size.X.Offset;
+                local gPos = Fill.AbsoluteSize.X;
                 local Diff = mPos - (Fill.AbsolutePosition.X + gPos);
 
                 while InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
@@ -2227,7 +2227,7 @@ do
                     local OldValue = Slider.Value;
                     Slider.Value = nValue;
 
-                    Slider:Display();
+                    Slider:Display(true);
 
                     if nValue ~= OldValue then
                         Library:SafeCallback(Slider.Callback, Slider.Value);
@@ -2407,7 +2407,7 @@ do
             SliderInner.InputBegan:Connect(function(Input)
                 if Input.UserInputType == Enum.UserInputType.MouseButton1 and not Library:MouseIsOverOpenedFrame() then
                     local mPos = Mouse.X;
-                    local gPos = Fill.Size.X.Offset;
+                    local gPos = Fill.AbsoluteSize.X;
                     local Diff = mPos - (Fill.AbsolutePosition.X + gPos);
 
                     while InputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
@@ -2430,7 +2430,10 @@ do
                 end;
             end);
 
-            Slider:Display(true);
+            task.defer(function()
+                Slider.MaxSize = math.max(1, SliderOuter.AbsoluteSize.X - 2);
+                Slider:Display(true);
+            end);
             Options[Idx] = Slider;
             return Slider;
         end;
