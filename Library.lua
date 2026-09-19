f82daf035c270bb02ac13e8ca2a8f49135d6e325
fakeposition <emblemaes@gmail.com>
2026-09-20 00:33:32 +0900

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2354,7 +2354,7 @@ do
             -- MaxSizeをAbsoluteSizeから動的に同期
             SliderOuter:GetPropertyChangedSignal('AbsoluteSize'):Connect(function()
                 Slider.MaxSize = math.max(1, SliderOuter.AbsoluteSize.X - 2);
-                Slider:Display(true);
+                Slider:Display();
             end);
 
             function Slider:UpdateColors()
@@ -2362,21 +2362,13 @@ do
                 Fill.BorderColor3     = Library.AccentColorDark;
             end;
 
-            function Slider:Display(Instant)
+            function Slider:Display()
                 local Suffix = Info.Suffix or '';
                 -- Compactスタイル: "Text: value"
                 DisplayLabel.Text = Info.Text .. ': ' .. Slider.Value .. Suffix;
 
                 local X = math.ceil(Library:MapValue(Slider.Value, Slider.Min, Slider.Max, 0, Slider.MaxSize));
-
-                if Instant then
-                    Fill.Size = UDim2.new(0, X, 1, 0);
-                else
-                    TweenService:Create(Fill, TweenInfo.new(0.08, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
-                        Size = UDim2.new(0, X, 1, 0);
-                    }):Play();
-                end
-
+                Fill.Size = UDim2.new(0, X, 1, 0);
                 HideBorderRight.Visible = not (X == Slider.MaxSize or X == 0);
             end;
 
@@ -2430,7 +2422,7 @@ do
                 end;
             end);
 
-            Slider:Display(true);
+            Slider:Display();
             Options[Idx] = Slider;
             return Slider;
         end;
@@ -3947,3 +3939,4 @@ Players.PlayerRemoving:Connect(OnPlayerChange);
 
 getgenv().Library = Library
 return Library
+sssssssssssssssssss
